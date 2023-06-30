935ed5fb1940b1a3b76faf281c1dd044d1f5290f
Hipstershiba <vitorrizzato@gmail.com>
2023-06-30 16:48:55 -0300

    stages implementation

--- sketch.js
+++ sketch.js
@@ -47,6 +47,11 @@ var seno = 0;
 var tetax;
 var tetay;
 
+var ycontrol = 0;
+var radiuscontrol = 0;
+
+var stage = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60);
@@ -78,42 +83,53 @@ function setup() {
   anchor4Y = height / 2;
 
   tetax = HALF_PI;
-  tetay = PI;
+  tetay = HALF_PI;
 }
 
 function draw() {
   background(255);
 
-  line(0, height / 2, width, height / 2);
-
-  desenharBezier();
+  // line(0, height / 2, width, height / 2);  
 
   // showHandles();
 
-  atualizarPontos(incremento);
+  // if (stage == 0)
+  // {
+  //   atualizaCirculo(1);
+  //   desenharCirculo(0, 0);
+  // } else if (stage == 1) {
+  //   atualizarPontos();
+  //   desenharBezier();
+  //   radiuscontrol = 500;
+  //   ycontrol = 198;
+  // } else {
+  //   background(0);
+  //   anchor2Y = (height / 2) + radius;
+  //   height_correction = 0;
+  //   tetax = PI;
+  //   atualizaCirculo(-1);
+  //   desenharCirculo(255, height / 2);
+  // }
 
-  var ycontrol = 320;
-  var radiuscontrol = 500;
-
-  push();
-  fill(0,0,175,100);
-  circle(width / 2, height / 2 - ycontrol, 100 * 2 + radiuscontrol);
-  pop();
-
-  incremento += 0;
+  atualizarPontos();
+  desenharBezier();
+  radiuscontrol = 500;
+  ycontrol = 320;
+  atualizaCirculo(1);
+  desenharCirculo(255, 0);
+  
   cosseno += PI/(60*60);
   seno += 2/(60*60);
-
-  tetax += TWO_PI/(60*5);
-  tetay += TWO_PI/(60*5);
 }
 
-function atualizarPontos(_incremento) {
-  _incremento = 250
+function atualizarPontos() {
+  tetax = (PI * 1) + HALF_PI;
+
+  var _incremento = 250
 
   radius = 100 + _incremento;
 
-  height_correction = 320;
+  var height_correction = 380 - (sin(tetax) * (60));
 
   control = radius * 0.551915024494;
 
@@ -123,9 +139,6 @@ function atualizarPontos(_incremento) {
   {
     sine = -1;
   }
-
-  // anchor0X = (width / 2) - radius;
-  // anchor0Y = (height / 2);
 
   anchor0X = 0;
   anchor0Y = 0;
@@ -150,10 +163,18 @@ function atualizarPontos(_incremento) {
   anchor4X = width;
   anchor4Y = 0;
 
+  // tetax += TWO_PI/(60*2.5);
+  tetay += TWO_PI/(60*2.5);
+
+  if (tetax >= PI + HALF_PI)
+  {
+    stage = 2;
+  }
 }
 
 function desenharBezier() {
   fill(255,255,0,100);
+  // fill(0);
   stroke(0);
   beginShape();
   vertex(anchor0X, anchor0Y);
@@ -190,4 +211,21 @@ function showHandles() {
   circle(anchor4X_control1, anchor4Y_control1, 10);
   fill(0, 0, 255);
   circle(anchor4X_control2, anchor4Y_control2, 10);
+}
+
+function atualizaCirculo(_sine) {
+  ycontrol += 320/(60*1);
+  radiuscontrol += (500 / (60*1)) * _sine;
+  if (ycontrol >= 320)
+  {
+    stage = 1;
+  }
+}
+
+function desenharCirculo(_fill, _stageMod) {
+  push();
+  fill(0,0,175,100);
+  // fill(_fill);
+  circle(width / 2, height / 2 - (ycontrol * 1) + _stageMod, 200 + (radiuscontrol * 1));
+  pop();
 }
