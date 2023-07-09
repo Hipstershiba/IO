1737d914888742668e53884730a32cc066461be2
Hipstershiba <vitorrizzato@gmail.com>
2023-07-09 10:17:30 -0300

    A animação da altura funciona, falta resolver a do raio

--- sketch.js
+++ sketch.js
@@ -1,52 +1,49 @@
 let frame_rate_value;
 
-var initialRadius;
-var radius;
+let initialRadius;
+let radius;
 
-var HandleDistance;
+let HandleDistance;
 
-var anchor0X;
-var anchor0Y;
+let anchor0X;
+let anchor0Y;
 
-var anchor1X;
-var anchor1Y;
+let anchor1X;
+let anchor1Y;
 
-var anchor2X;
-var anchor2Y;
-var anchor2X_control1;
-var anchor2Y_control1;
-var anchor2X_control2;
-var anchor2Y_control2;
+let anchor2X;
+let anchor2Y;
+let anchor2X_control1;
+let anchor2Y_control1;
+let anchor2X_control2;
+let anchor2Y_control2;
 
-var anchor3X;
-var anchor3Y;
-var anchor3X_control1;
-var anchor3Y_control1;
-var anchor3X_control2;
-var anchor3Y_control2;
+let anchor3X;
+let anchor3Y;
+let anchor3X_control1;
+let anchor3Y_control1;
+let anchor3X_control2;
+let anchor3Y_control2;
 
-var anchor4X;
-var anchor4Y;
-var anchor4X_control1;
-var anchor4Y_control1;
-var anchor4X_control2;
-var anchor4Y_control2;
+let anchor4X;
+let anchor4Y;
+let anchor4X_control1;
+let anchor4Y_control1;
+let anchor4X_control2;
+let anchor4Y_control2;
 
-var incremento = 0;
+let incremento;
 
-var teta;
+let theta;
 
-var heightControl = 0;
-var radiusControl = 0;
+let heightControl;
+let radiusControl;
 
 let animation_Cicle_Duration_In_Seconds;
 let animation_Cicle_Duration_In_Frames;
 
 let frame_oddness;
 let cicle_count;
-
-let circle_color = 0;
-let background_color = 255;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -81,7 +78,10 @@ function setup() {
   anchor4X = width / 2 + radius;
   anchor4Y = height / 2;
 
-  teta = HALF_PI;
+  theta = HALF_PI;
+
+  heightControl = -radius;
+  radiusControl = 0;
 
   animation_Cicle_Duration_In_Seconds = 3;
   animation_Cicle_Duration_In_Frames = frame_rate_value * animation_Cicle_Duration_In_Seconds;
@@ -92,49 +92,42 @@ function setup() {
 
 function draw() {
   
-  line(0, height / 2, width, height / 2);
-  line(0, height / 2 + radius, width, height / 2 + radius);
-  line(0, height / 2 + (radius * 2), width, height / 2 + (radius * 2));
-  line(width / 2 - (radius), 0, width / 2 - (radius), height);
+  if (animation_cicle_ended()) {
+    // noLoop();
+    theta = HALF_PI;
+    cicle_count += 1;
+    reinicia_altura();
+  }
+  
+  background(atualiza_cor_de_fundo());
+  fill(atualiza_cor_do_circulo());
 
-  if (frameCount % animation_Cicle_Duration_In_Frames <= animation_Cicle_Duration_In_Frames / 3)
+  atualiza_altura();
+  atualiza_raio();
+  // radius = radiusControl;
+
+  if (frameCount % animation_Cicle_Duration_In_Frames <= (animation_Cicle_Duration_In_Frames / 3) * 2)
   {
-    teta = HALF_PI;
-
-    background(background_color);
-    drawCircle(circle_color, 0);
-  } else if (frameCount % animation_Cicle_Duration_In_Frames <= (animation_Cicle_Duration_In_Frames / 3) * 2) {
+    drawCircle(0);
+  } else {
     atualizarPontos();
     desenharBezier();
-  } else if (frameCount % animation_Cicle_Duration_In_Frames < animation_Cicle_Duration_In_Frames) {
-    changeColors();
-    background(background_color);
-
-    drawCircle(circle_color, initialRadius * 2);
-  } else {
-    heightControl = 0;
-    cicle_count += 1;
   }
-
-  if (animationEnded())
-  {
-    heightControl = 0;
-    cicle_count += 1;
-  }
-
-  heightControl += 1;
-
-  // atualizarPontos();
-  // desenharBezier();
-  // drawCircle(255, initialRadius * 2);
-
-  // showHandles();
 
   if(mouseX > width / 2)
   {
     showHandles();
-    heightControl = mouseY - (height / 2);
   }
+
+  stroke(255,255,0);
+  mostra_linha_de_controle();
+}
+
+function mostra_linha_de_controle() {
+  line(0, height / 2, width, height / 2);
+  line(0, height / 2 + radius, width, height / 2 + radius);
+  line(0, height / 2 + (radius * 2), width, height / 2 + (radius * 2));
+  line(width / 2 - (radius), 0, width / 2 - (radius), height);
 }
 
 function atualizarPontos() {
@@ -143,35 +136,33 @@ function atualizarPontos() {
   anchor0X = 0;
   anchor0Y = 0;
 
-  anchor1X = (width / 2) - radius + (cos(teta) * ((HALF_PI / 2) * radius));
-  anchor1Y = (height / 2) + radius - (sin(teta) * (radius)) - heightControl;
+  anchor1X = (width / 2) - radius + (cos(theta) * ((HALF_PI / 2) * radius));
+  anchor1Y = (height / 2) + radius - (sin(theta) * (radius)) - heightControl;
 
   anchor2X = (width / 2);
   anchor2Y = (height / 2) + radius - heightControl;
-  anchor2X_control1 = (width / 2) - radius + (cos(teta) * (1));
-  anchor2Y_control1 = (height / 2) + radius - (sin(teta) * (radius - HandleDistance)) - heightControl;
+  anchor2X_control1 = (width / 2) - radius + (cos(theta) * (1));
+  anchor2Y_control1 = (height / 2) + radius - (sin(theta) * (radius - HandleDistance)) - heightControl;
   anchor2X_control2 = (width / 2) - HandleDistance;
   anchor2Y_control2 = (height / 2) + radius - heightControl;
 
-  anchor3X = (width / 2) + radius -(cos(teta) * ((HALF_PI / 2) * radius));
-  anchor3Y = (height / 2) + radius - (sin(teta) * (radius))  - heightControl;
+  anchor3X = (width / 2) + radius -(cos(theta) * ((HALF_PI / 2) * radius));
+  anchor3Y = (height / 2) + radius - (sin(theta) * (radius))  - heightControl;
   anchor3X_control1 = (width / 2) + HandleDistance;
   anchor3Y_control1 = (height / 2) + radius - heightControl;
-  anchor3X_control2 = (width / 2) + radius + (cos(teta) * (1));
-  anchor3Y_control2 = (height / 2) + radius - (sin(teta) * (radius - HandleDistance)) - heightControl;
+  anchor3X_control2 = (width / 2) + radius + (cos(theta) * (1));
+  anchor3Y_control2 = (height / 2) + radius - (sin(theta) * (radius - HandleDistance)) - heightControl;
 
   anchor4X = width;
   anchor4Y = 0;
 
-  if(frameCount % animation_Cicle_Duration_In_Frames <= (animation_Cicle_Duration_In_Frames / 3) * 2 || frameCount >= animation_Cicle_Duration_In_Frames)
+  if(frameCount % animation_Cicle_Duration_In_Frames > (animation_Cicle_Duration_In_Frames / 3) * 2)
   {
-    teta += PI / (animation_Cicle_Duration_In_Frames / 3);
+    theta += PI / (animation_Cicle_Duration_In_Frames / 3);
   }
 }
 
 function desenharBezier() {
-  fill(255,255,0,100);
-  // fill(0);
   stroke(0);
   beginShape();
   vertex(anchor0X, anchor0Y);
@@ -183,6 +174,7 @@ function desenharBezier() {
 }
 
 function showHandles() {
+  push();
   fill(0, 50, 0);
   circle(anchor0X, anchor0Y, 10);
   fill(0, 100, 0);
@@ -208,32 +200,59 @@ function showHandles() {
   circle(anchor4X_control1, anchor4Y_control1, 10);
   fill(0, 0, 255);
   circle(anchor4X_control2, anchor4Y_control2, 10);
-}
-
-function drawCircle(_fill, _height_deslocation) {
-  push();
-  fill(0,0,175,100);
-  fill(_fill);
-  circle(width / 2, height / 2 - (heightControl * 1) + _height_deslocation, radius * 2);
   pop();
 }
 
-function changeColors() {
+function drawCircle() {
+  circle(width / 2, height / 2 - (heightControl * 1), radius * 2);
+}
+
+function atualiza_cor_de_fundo() {
   if (cicle_count % 2 == 0)
   {
-    background_color = 0;
-    circle_color = 255;
+    return 255;
   } else  {
-    background_color = 255;
-    circle_color = 0;
+    return 0;
+  }
+}
+
+function atualiza_cor_do_circulo() {
+  if (cicle_count % 2 == 0) {
+    return 0;
+  } else {
+    return 255;
   }
 }
 
 //função que detecta se o ciclo de animação acabou
-function animationEnded() {
-  if (frameCount >= animation_Cicle_Duration_In_Frames * 2) {
+function animation_cicle_ended() {
+  if (frameCount % animation_Cicle_Duration_In_Frames == 0) {
     return true;
   } else {
     return false;
   }
+}
+
+function atualiza_raio() {
+  if (frameCount % animation_Cicle_Duration_In_Frames > animation_Cicle_Duration_In_Frames / 2) {
+    decreaseRadius();
+  } else {
+    increaseRadius();
+  }
+}
+
+function increaseRadius() {
+  radiusControl += (width / 2) / (animation_Cicle_Duration_In_Frames / 3) * 2;
+}
+
+function decreaseRadius() {
+  radiusControl -= (width / 2) / (animation_Cicle_Duration_In_Frames / 3) * 2;
+}
+
+function atualiza_altura() {
+  heightControl += (radius * 2) / animation_Cicle_Duration_In_Frames;
+}
+
+function reinicia_altura() {
+  heightControl = -radius;
 }
